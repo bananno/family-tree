7a025b2b73ff41cdbae1ccf3674abe47f2450ce7
Anna Bidstrup <annabidstrup20@gmail.com>
2023-08-09 10:44:39 -0400

    Split citation item info into two parts

--- src/CitationList.jsx
+++ src/CitationList.jsx
@@ -5,22 +5,24 @@ import SourceLink from './SourceLink';
 import classes from './CitationList.module.scss';
 
 function CitationList({citations, showPerson = true, showSource = true}) {
-  let previousPersonId, previousItem, previousInfo;
+  let previousPersonId, previousItem1, previousItem2, previousInfo;
   return (
     <table className={classes.CitationList}>
       <thead>
         <tr>
-          <th>item</th>
+          {showPerson && <th>person</th>}
+          <th colSpan="2">item</th>
           <th>information</th>
-          <th>source</th>
+          {showSource && <th>source</th>}
         </tr>
       </thead>
       <tbody>
         {citations.map((citation, i) => {
-          let displayPersonLink, displayItemText, displayInformationText;
+          let startPersonSection, startItem1Section, startItem2Section, displayInformationText;
 
           const personClasses = [classes.borderLeft];
-          const itemClasses = [];
+          const itemClasses1 = [];
+          const itemClasses2 = [];
           const informationClasses = [];
           const sourceClasses = [classes.borderTop, classes.borderRight];
 
@@ -28,17 +30,23 @@ function CitationList({citations, showPerson = true, showSource = true}) {
             personClasses.push(classes.borderLeft);
             if (previousPersonId !== citation.person.id) {
               previousPersonId = citation.person.id;
-              displayPersonLink = true;
-              itemClasses.push(classes.borderTop);
+              startPersonSection = true;
+              personClasses.push(classes.borderTop);
             }
           } else {
-            itemClasses.push(classes.borderLeft);
+            itemClasses1.push(classes.borderLeft);
           }
 
-          if (previousItem !== citation.item) {
-            previousItem = citation.item;
-            displayItemText = true;
-            itemClasses.push(classes.borderTop);
+          if (previousItem1 !== citation.itemPart1 || startPersonSection) {
+            previousItem1 = citation.itemPart1;
+            startItem1Section = true;
+            itemClasses1.push(classes.borderTop);
+          }
+
+          if (previousItem2 !== citation.itemPart2 || startItem1Section) {
+            previousItem2 = citation.itemPart2;
+            startItem2Section = true;
+            itemClasses2.push(classes.borderTop);
           }
 
           if (previousInfo !== citation.information) {
@@ -53,7 +61,8 @@ function CitationList({citations, showPerson = true, showSource = true}) {
 
           if (i === citations.length - 1) {
             personClasses.push(classes.borderBottom);
-            itemClasses.push(classes.borderBottom);
+            itemClasses1.push(classes.borderBottom);
+            itemClasses2.push(classes.borderBottom);
             informationClasses.push(classes.borderBottom);
             sourceClasses.push(classes.borderBottom);
           }
@@ -62,10 +71,13 @@ function CitationList({citations, showPerson = true, showSource = true}) {
           return (
             <tr key={citation.id}>
               {showPerson && <td className={personClasses.join(' ')}>
-                {displayPersonLink && <PersonLink person={citation.person}/>}
+                {startPersonSection && <PersonLink person={citation.person}/>}
               </td>}
-              <td className={itemClasses.join(' ')}>
-                {displayItemText && citation.item}
+              <td className={itemClasses1.join(' ')}>
+                {startItem1Section && citation.itemPart1}
+              </td>
+              <td className={itemClasses2.join(' ')}>
+                {startItem2Section && citation.itemPart2}
               </td>
               <td className={informationClasses.join(' ')}>
                 {displayInformationText && citation.information}
