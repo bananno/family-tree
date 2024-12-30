cc13a355c2f9bade85c0e19773f9afa44ba5118c
Anna Bidstrup <annabidstrup20@gmail.com>
2024-12-30 15:38:05 -0500

    Show list of uploaded files; add custom filename

--- publish.sh
+++ publish.sh
@@ -4,9 +4,9 @@ echo "Build app"
 npm run build
 
 echo "\nUpload to S3"
-aws s3 sync dist/ s3://$S3_BUCKET_NAME --delete
+aws s3 sync dist/ s3://$DEPLOYMENT_S3_BUCKET_NAME --delete
 
 echo "\nInvalidate CloudFront cache"
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
+aws cloudfront create-invalidation --distribution-id $DEPLOYMENT_CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
 
 echo "\nDone"
--- src/file/components/UploadFileForm.jsx
+++ src/file/components/UploadFileForm.jsx
@@ -5,9 +5,12 @@ const API_URL = 'http://localhost:9000';
 export default function UploadFileForm() {
   const [file, setFile] = useState(null);
   const [submitting, setSubmitting] = useState(false);
+  const [filename, setFilename] = useState('');
 
   async function handleSelectFile(event) {
-    setFile(event.target.files[0]);
+    const file = event.target.files[0];
+    setFile(file);
+    setFilename(file.name.replace(/ /g, '_'));
   }
 
   async function handleSubmit(event) {
@@ -19,11 +22,12 @@ export default function UploadFileForm() {
 
     const formData = new FormData();
     formData.append('file', file);
+    formData.append('filename', filename);
 
     setSubmitting(true);
 
     try {
-      await fetch(`${API_URL}/file/upload`, {
+      await fetch(`${API_URL}/files`, {
         method: 'POST',
         body: formData,
       });
@@ -45,9 +49,30 @@ export default function UploadFileForm() {
         accept="image/*"
         disabled={submitting}
       />
-      <button type="submit" disabled={submitting}>
-        Upload
-      </button>
+      {file && (
+        <>
+          <br />
+          <br />
+          Original name:
+          <br />
+          <i>{file?.name}</i>
+          <br />
+          <br />
+          Filename and directory: <br />
+          <input
+            type="text"
+            value={filename}
+            onChange={e => setFilename(e.target.value)}
+            disabled={submitting}
+            style={{ width: '500px' }}
+          />
+          <br />
+          <br />
+          <button type="submit" disabled={submitting}>
+            Upload
+          </button>
+        </>
+      )}
     </form>
   );
 }
