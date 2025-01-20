3f615b4e2fcee4f6c9f79543d70bcf4eaa8d2d16
Anna Bidstrup <annabidstrup20@gmail.com>
2025-01-20 13:17:21 -0500

    Begin creating a new route for managing person links

---
+++ app/person/updatePersonLinks.route.js
@@ -0,0 +1,54 @@
+import Person from './Person.model.js';
+
+export default async function updatePersonLinksRoute(req, res) {
+  const person = await Person.findById(req.params.id);
+
+  if (!person) {
+    return res.status(404).send('Person not found');
+  }
+
+  const action = req.body.action;
+  const index = req.body.index;
+
+  if (!['add', 'update', 'delete', 'reorder'].includes(action)) {
+    return res.status(400).send('Invalid action');
+  }
+
+  let newLinkValue;
+  if (['add', 'update'].includes(action)) {
+    const url = req.body.url.trim();
+    const text = req.body.text.trim();
+    if (!url) {
+      return res.status(400).send('URL is required');
+    }
+    newLinkValue = text ? `${url} ${text}` : url;
+  }
+
+  if (action !== 'add') {
+    if (person.links[index] === undefined) {
+      return res.status(400).send('Valid index is required');
+    }
+    if (action === 'reorder' && person.links[index - 1]) {
+      return res.status(400).send('Valid index is required');
+    }
+  }
+
+  if (action === 'delete') {
+    person.links = [
+      ...person.links.slice(0, index),
+      ...person.links.slice(index + 1),
+    ];
+  } else if (action === 'update') {
+    person.links[index] = newLinkValue;
+  } else if (action === 'reorder') {
+    const tempLink = person.links[index];
+    person.links[index] = person.links[index - 1];
+    person.links[index - 1] = tempLink;
+  } else {
+    person.links.push(newLinkValue);
+  }
+
+  await person.save();
+
+  res.send();
+}
---
+++ test/updatePersonLinks.route.test.js
@@ -0,0 +1,258 @@
+import './testDb.js';
+import { res } from './testTools.js';
+
+import updatePersonLinksRoute from '../app/person/updatePersonLinks.route';
+import Person from '../app/person/Person.model';
+
+const personId = '5bce02694df8a32e9026e654';
+
+const req = { params: { id: personId }, body: {} };
+
+async function callRoute() {
+  await updatePersonLinksRoute(req, res);
+}
+
+describe('updatePersonLinksRoute', () => {
+  beforeEach(async () => {
+    await Person.deleteMany();
+    await Person.create({
+      _id: personId,
+      links: [
+        'https://www.ancestry.com/456 Ancestry',
+        'https://www.familysearch.org/789 FamilySearch',
+        'https://www.example.com/123 Example',
+      ],
+    });
+  });
+
+  describe('when person is not found', () => {
+    beforeEach(async () => {
+      req.params.id = 'df8a396e65442e9025bce026';
+      await callRoute();
+    });
+
+    afterEach(() => {
+      req.params.id = personId;
+    });
+
+    it('returns a 404 status', () => {
+      expect(res.status).toHaveBeenCalledWith(404);
+    });
+  });
+
+  describe('when action is "add"', () => {
+    beforeEach(async () => {
+      req.body = {
+        action: 'add',
+        url: 'https://www.findagrave.com/123456',
+        text: 'FindAGrave',
+      };
+    });
+
+    describe('when the body contains url and text', () => {
+      beforeEach(async () => {
+        await callRoute();
+      });
+
+      it('adds the link', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([
+          'https://www.ancestry.com/456 Ancestry',
+          'https://www.familysearch.org/789 FamilySearch',
+          'https://www.example.com/123 Example',
+          'https://www.findagrave.com/123456 FindAGrave',
+        ]);
+      });
+    });
+
+    describe('when url is missing', () => {
+      beforeEach(async () => {
+        req.body.url = ' ';
+        await callRoute();
+      });
+
+      it('returns a 400 status', () => {
+        expect(res.status).toHaveBeenCalledWith(400);
+      });
+    });
+
+    describe('when text is missing', () => {
+      beforeEach(async () => {
+        req.body.text = '';
+        await callRoute();
+      });
+
+      it('adds the link without text', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([
+          'https://www.ancestry.com/456 Ancestry',
+          'https://www.familysearch.org/789 FamilySearch',
+          'https://www.example.com/123 Example',
+          'https://www.findagrave.com/123456',
+        ]);
+      });
+    });
+  });
+
+  describe('when action is "update"', () => {
+    beforeEach(async () => {
+      req.body = {
+        action: 'update',
+        index: 1,
+        url: 'https://www.familysearch.org/789/updated',
+        text: 'FamilySearchUpdated',
+      };
+    });
+
+    describe('when the body contains url and text', () => {
+      beforeEach(async () => {
+        await callRoute();
+      });
+
+      it('updates the link', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([
+          'https://www.ancestry.com/456 Ancestry',
+          'https://www.familysearch.org/789/updated FamilySearchUpdated',
+          'https://www.example.com/123 Example',
+        ]);
+      });
+    });
+
+    describe('when url is missing', () => {
+      beforeEach(async () => {
+        req.body.url = ' ';
+        await callRoute();
+      });
+
+      it('returns a 400 status', () => {
+        expect(res.status).toHaveBeenCalledWith(400);
+      });
+    });
+
+    describe('when text is missing', () => {
+      beforeEach(async () => {
+        req.body.text = '';
+        await callRoute();
+      });
+
+      it('updates the link without text', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([
+          'https://www.ancestry.com/456 Ancestry',
+          'https://www.familysearch.org/789/updated',
+          'https://www.example.com/123 Example',
+        ]);
+      });
+    });
+
+    describe('when the index is 0', () => {
+      beforeEach(async () => {
+        req.body = {
+          action: 'update',
+          index: 0,
+          url: 'https://www.ancestry.com/456/updated',
+          text: 'AncestryUpdated',
+        };
+        await callRoute();
+      });
+
+      it('updates the link', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([
+          'https://www.ancestry.com/456/updated AncestryUpdated',
+          'https://www.familysearch.org/789 FamilySearch',
+          'https://www.example.com/123 Example',
+        ]);
+      });
+    });
+
+    describe('when the index is invalid', () => {
+      beforeEach(async () => {
+        req.body.index = 33;
+        await callRoute();
+      });
+
+      it('returns a 400 status', () => {
+        expect(res.status).toHaveBeenCalledWith(400);
+      });
+    });
+  });
+
+  describe('when action is "delete"', () => {
+    beforeEach(async () => {
+      req.body = {
+        action: 'delete',
+        index: 1,
+      };
+    });
+
+    describe('when the index is 0', () => {
+      beforeEach(async () => {
+        req.body.index = 0;
+        await callRoute();
+      });
+
+      it('deletes the link', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([
+          'https://www.familysearch.org/789 FamilySearch',
+          'https://www.example.com/123 Example',
+        ]);
+      });
+    });
+
+    describe('when the index is greater than 0', () => {
+      beforeEach(async () => {
+        req.body.index = 2;
+        await callRoute();
+      });
+
+      it('deletes the link', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([
+          'https://www.ancestry.com/456 Ancestry',
+          'https://www.familysearch.org/789 FamilySearch',
+        ]);
+      });
+    });
+
+    describe('when there is only one link in the list', () => {
+      beforeEach(async () => {
+        await Person.updateOne(
+          {},
+          { links: ['https://www.familysearch.org/789 FamilySearch'] },
+        );
+        req.body.index = 0;
+        await callRoute();
+      });
+
+      it('deletes the link', async () => {
+        const person = await Person.findById(personId);
+        expect(person.links).toEqual([]);
+      });
+    });
+
+    describe('when the index is invalid', () => {
+      beforeEach(async () => {
+        req.body.index = 33;
+        await callRoute();
+      });
+
+      it('returns a 400 status', () => {
+        expect(res.status).toHaveBeenCalledWith(400);
+      });
+    });
+  });
+
+  describe('when the action is invalid', () => {
+    beforeEach(async () => {
+      req.body.action = 'other';
+      await callRoute();
+    });
+
+    it('returns a 400 status', () => {
+      expect(res.status).toHaveBeenCalledWith(400);
+    });
+  });
+});
