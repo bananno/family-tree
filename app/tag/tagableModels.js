1dc4e66b8fd98533528a5097e4b8bee39545a141
Anna Bidstrup <annabidstrup20@gmail.com>
2025-05-26 10:48:30 -0400

    Delete old tag routes and clean up constants

--- app/tag/tagableModels.js
+++ app/tag/tagableModels.js
@@ -1,13 +1,3 @@
-import Event from '../event/Event.model.js';
-import Image from '../image/Image.model.js';
-import Notation from '../notation/Notation.model.js';
-import Person from '../person/Person.model.js';
-import Source from '../source/Source.model.js';
-import Story from '../story/Story.model.js';
-import Tag from './Tag.model.js';
-
-// export const TAGABLE_MODELS = [Event, Image, Notation, Person, Source, Story, Tag];
-
 const TAGABLE_MODELS = [
   { name: 'Event', plural: 'events' },
   { name: 'Image', plural: 'images' },
